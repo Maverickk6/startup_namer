6ed336547a505cf0e6326c4671b2a54be7bdbfe3
System Administrator <eddymav247@gmail.com>
2020-03-27 17:39:49 +0100

    task-commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,125 +7,28 @@ class MyApp extends StatelessWidget {
   // #docregion build
   @override
   Widget build(BuildContext context) {
+    // var details = [
+    //   'Name: Edidiong Ekpo'
+    // ]
     return MaterialApp(
-      title: 'Startup Name Generator',
+      title: 'My Details',
       theme: ThemeData(
-        primaryColor: Colors.grey[300],
+        primaryColor: Colors.blue[300],
       ),
-      home: RandomWords(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My Bio'),
+        ),
+        body: Column(children: [
+          Text('Name: Edidiong Ekpo'),
+          Text('Slack Username: Maverick'),
+          Text('Start.Ng Email: Eddymav247@gmail.com'),
+          Text('Courses: Flutter, Javscript Html And Css, UI/UX design')
+        ],),
+
+      ),
     );
   }
   // #enddocregion build
 }
 // #enddocregion MyApp
-
-// #docregion RWS-var
-class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final Set<WordPair> _saved = Set<WordPair>();
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-  
-  // #enddocregion RWS-var
-
-  // #docregion _buildSuggestions
- 
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return Divider(); /*2*/
-
-          final index = i ~/ 2; /*3*/
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-  // #enddocregion _buildSuggestions
-
-  // #docregion _buildRow
-  Widget _buildRow(WordPair pair) {
-    final bool alreadySaved = _saved.contains(pair);
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null, ),
-        onTap: () {
-          setState (() {
-            if (alreadySaved) {
-              _saved.remove(pair);
-            } else {
-              _saved.add(pair);
-            }
-          });
-        },
-    );
-  }
-  // #enddocregion _buildRow
-
-  // #docregion RWS-build
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
-        actions: <Widget>[
-        IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
-      ],
-      ),
-      body: _buildSuggestions(),
-      
-      );
-     
-  }
-  
-  
-  // #enddocregion RWS-build
-  // #docregion RWS-var
-
-// #enddocregion RWS-var
-void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        // Add 20 lines from here...
-        builder: (BuildContext context) {
-          final tiles = _saved.map(
-            (WordPair pair) {
-              return ListTile(
-                title: Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final divided = ListTile.divideTiles(
-            context: context,
-            tiles: tiles,
-          ).toList();
-
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('Saved Suggestions'),
-            ),
-            body: ListView(children: divided),
-          );
-        },
-      ),
-    );
-  }
-}
-
-
-
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => new RandomWordsState();
-}
-
-
